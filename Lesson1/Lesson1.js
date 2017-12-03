353e77b1b43a0187c5d177294ddc81ec32f53db1
Dmitriy Kornev <silentspec84@mail.ru>
2017-12-03 10:58:58 +0300

    jQuery Lesson 1 + Lesson 2

---
+++ Lesson1/Lesson1.js
@@ -0,0 +1,12 @@
+$('#btn_start').click(function(){
+	$("#message")
+		.html("Message")
+		.css("background-color","Red")
+		.parent()
+		.css("background-color","fff4dd")
+		.width(150)
+		.height(80)	
+});
+$('#btn_reset').click(function(){
+	location.reload();
+});
---
+++ Lesson2/Lesson2.js
@@ -0,0 +1,13 @@
+// css
+
+// $("#content").css("color","red")
+// $(".wrapper").css("color","red")
+// $("h1,h2").css("color","red")
+// $("article h2").css("color","red")
+// $("article").find("h2").css("color","red")
+// $("h1+p").css("color","red")
+// $("p").prev().css("color","red")
+// $("h2").next().css("color","red")
+// $("*").next().css("color","red")
+// $("article > h2").next().css("color","red")
+// $("article > *").next().css("color","red")
